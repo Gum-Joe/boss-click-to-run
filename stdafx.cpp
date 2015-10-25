89b97f9e253457b625c1c2a45323f79f2d985439
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-10-25 16:42:19 +0000

    Add first files

---
+++ stdafx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp : source file that includes just the standard includes
+// Web-OS-click-to-run.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+
